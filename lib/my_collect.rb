cd4f53d4d3f826429c7d5823b9824d0e0723b85d
Amy Cohen <amy.cohen@flatironschool.com>
2019-04-15 15:47:52 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+require 'pry'
 
+def my_collect(array_info)
+    if array_info.length == 0
+        return array_info
+    end
 
+    count = 0
+    modified_array = []
+    modified_entry = nil
+
+    while count < array_info.length
+        modified_array << yield(array_info[count])
+        count += 1
+    end
+    puts "Modified Array = #{modified_array}"
+    modified_array
+end
